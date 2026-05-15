e88dcd5f523f33967defbc518b6c30763362e46a
Callum Wigston <callum.wigston@my.bdsc.school.nz>
2026-05-15 13:06:10 +1200

    Started data file saving, with saving data to the file and loading data from file.

--- workout_helper.py
+++ workout_helper.py
@@ -7,4 +7,40 @@ WORKOUT_FILE = "Workouts.txt"
 # This list will hold all the workouts as dictionaries
 workouts = []
 
+"""
+    This def tries to open the workout file and load data into the list.
+    If the file doesn't exist yet, the list will stay empty.
+    Each line in the file is stored as: type, date, amount, unit
+"""
+def load_workouts():
+    try:# This will try to open and read the workout file
+        file = open(WORKOUT_FILE, "r")
+        lines = file.readlines()
+        file.close()
+
+ # This will loop through each line and split it into a dictionary
+        for line in lines:
+            line = line.strip()
+            if line != "":
+                parts = line.split("|")
+                workout = {
+                    "type":   parts[0],
+                    "date":   parts[1],
+                    "amount": parts[2],
+                    "unit":   parts[3]
+                }
+                workouts.append(workout)
+    except FileNotFoundError:  # If the file doesn't exist yet, the list will stay empty
+        workouts = []
+
+"""
+    Saves all workouts from the list to the text file. Each workout will be saved in one line as: type,date,amount,unit
+"""
+def save_workouts():
+    file = open(WORKOUT_FILE, "w")
+    for w in workouts:
+        line = w["type"] + "|" + w["date"] + "|" + w["amount"] + "|" + w["unit"] + "\n"
+        file.write(line)
+    file.close()
+ 
 
