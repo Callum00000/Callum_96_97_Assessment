dcc22bddf92afccf3c75da738f99e7562af4157a
Callum Wigston <callum.wigston@my.bdsc.school.nz>
2026-05-15 13:02:42 +1200

    Started workout program with tkinter imports

--- workout_helper.py
+++ workout_helper.py
@@ -1,1 +1,10 @@
-test
+import tkinter as tk
+from tkinter import messagebox
+
+# Name of the file that going to beused to store workout data
+WORKOUT_FILE = "Workouts.txt"
+
+# This list will hold all the workouts as dictionaries
+workouts = []
+
+
